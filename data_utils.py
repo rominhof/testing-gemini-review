525d32da09c7643a4fc915eba7d451ed53979d23
rominhof <romuloquidute@gmail.com>
2025-06-18 15:04:41 -0300

    Create data_utils.py

---
+++ data_utils.py
@@ -0,0 +1,9 @@
+def procces_data(dta):
+    resutl = []
+    for i in dta:
+        if i % 2 == 0:
+            resutl.append(i * 2)
+        else:
+            resutl.append(i + 1)
+    return resutl
+
